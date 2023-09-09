5959dcc04c148b5e8d613f7b81fb9de90606995b
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-09-09 20:21:16 +0300

    Done

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,31 @@
 from config import CONN, CURSOR
+import sqlite3
+
+CONN = sqlite3.connect('music.db')
+CURSOR = CONN.cursor()
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(self):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
+
+hello = Song("Hello", "25")
+hello.save()
+
+despacito = Song("Despacito", "Vida")
+despacito.save()
+songs = CURSOR.execute('SELECT * FROM songs')
+[row for row in songs]
