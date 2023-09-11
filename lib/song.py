c85d52a982be452d94211d3adce9d435ca5244c6
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-09-11 07:19:40 +0300

    Done

--- lib/song.py
+++ lib/song.py
@@ -11,7 +11,7 @@ class Song:
         self.album = album
 
     @classmethod
-    def create_table(self):
+    def create_table(cls):  # Updated method name to cls
         sql = """
             CREATE TABLE IF NOT EXISTS songs (
                 id INTEGER PRIMARY KEY,
@@ -21,11 +21,33 @@ class Song:
         """
 
         CURSOR.execute(sql)
+        
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
+    
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
 
+        CURSOR.execute(sql, (self.name, self.album))
+        self.id = CURSOR.lastrowid  # Updated this line
+
+# Create the table
+Song.create_table()
+
+# Now you can insert and retrieve data
 hello = Song("Hello", "25")
 hello.save()
 
 despacito = Song("Despacito", "Vida")
 despacito.save()
+
+# Retrieve data
 songs = CURSOR.execute('SELECT * FROM songs')
-[row for row in songs]
+for row in songs:
+    print(row)
